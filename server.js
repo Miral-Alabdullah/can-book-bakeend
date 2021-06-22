468cf0d6a0ebaf380e5535d1a4f57276509b5c80
Leen Hazaimeh <leenhazaimeh94@gmail.com>
2021-06-22 21:04:24 +0300

    done with lab 13 bakend

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 const express = require('express');
 const app = express();
-
 const mongoose = require('mongoose');
-
 require('dotenv').config();
-
 const PORT = process.env.PORT;
-
-const getBooks = require('./controllers/books.controller');
-const seedUserData = require('./models/user.model');
-
+const {getBooks, createBook, updateBook, deleteBook} = require('./controllers/books.controller');
+const seedUserData  = require('./models/user.model');
 const cors = require('cors');
 app.use(cors());
-
-
-
-
+app.use(express.json());
 mongoose.connect('mongodb://localhost:27017/myFavoriteBooks',
     { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
 
-// seedUserData();
 
-
-app.get('/books', getBooks)
-
+// seedUserData ();
+app.get('/books', getBooks);
+app.post('/book', createBook);
+app.put('/book/:book_idx', updateBook);
+app.delete('/book/:book_idx', deleteBook)
 app.listen(PORT);
+
+
+
+
+
